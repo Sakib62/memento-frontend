f8a29f3b9392f4a16dffc73f1278941b68c78f91
sakib62 <sakib62.coder@gmail.com>
2025-03-21 17:09:29 +0600

    refactor: storycard, storyview

--- src/components/StoryCard.tsx
+++ src/components/StoryCard.tsx
@@ -1,86 +1,103 @@
-import MarkdownIt from 'markdown-it';
 import { useContext, useEffect, useState } from 'react';
-import { FaBookmark, FaEllipsisV, FaHeart, FaRegComment } from 'react-icons/fa';
+import { FaRegComment, FaRegHeart } from 'react-icons/fa';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import MarkdownRenderer from './MarkdownRenderer';
 
 interface Story {
-  id: number;
+  id: string;
   title: string;
   description: string;
-  bannerUrl?: string | null; // Allow null or undefined
-  createdAt: string;
-  likes: number;
-  comments: number;
+  authorUsername: string;
+  authorName: string;
+  bannerUrl?: string | null;
+  createdAt: Date;
 }
 
 const StoryCard = ({ story }: { story: Story }) => {
   const authContext = useContext(AuthContext);
   if (!authContext?.token) {
-    console.log('lol');
     return <Navigate to='/login' />;
   }
   const { token } = authContext;
 
   const navigate = useNavigate();
-  const mdParser = new MarkdownIt();
-
-  const parsedDescription = mdParser.render(story.description);
 
   const handleClick = () => {
     navigate(`/story/${story.id}`, { state: story });
   };
 
-  const [likeCount, setLikeCount] = useState<number>(story.likes);
+  const [likeCount, setLikeCount] = useState<number>(0);
   const [isLiked, setIsLiked] = useState<boolean>(false);
+  const [commentCount, setCommentCount] = useState<number>(0);
+
+  const fetchLikeData = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/stories/${story.id}/likeStatus`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await response.json();
+      setLikeCount(data.data.likeCount);
+      setIsLiked(data.data.likedByUser);
+    } catch (error) {
+      console.error('Error fetching like data:', error);
+    }
+  };
+
+  const fetchCommentCount = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/stories/${story.id}/commentCount`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await response.json();
+      setCommentCount(data.data);
+    } catch (error) {
+      console.error('Error fetching comment count:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchLikeData = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:3000/api/stories/${story.id}/likeStatus`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        const data = await response.json();
-        setLikeCount(data.data.likeCount);
-        setIsLiked(data.data.likedByUser);
-      } catch (error) {
-        console.error('Error fetching like data:', error);
-      }
-    };
-
     fetchLikeData();
+    fetchCommentCount();
   }, [story.id]);
 
   return (
     <div
       onClick={handleClick}
-      className='flex items-stretch gap-6 p-6 transition-all duration-300 ease-in-out bg-gray-100 shadow-md cursor-pointer rounded-xl hover:shadow-lg dark:bg-gray-900'
+      className='flex items-stretch gap-6 p-6 transition-all duration-300 ease-in-out bg-gray-200 shadow-md cursor-pointer rounded-xl hover:shadow-lg dark:bg-stone-800'
     >
       {/* Left Section: Text Content (Full width if no banner) */}
       <div
         className={`flex flex-col justify-between ${story.bannerUrl ? 'flex-1' : 'w-full'}`}
       >
         <div>
-          <h3 className='text-4xl font-semibold text-gray-800 dark:text-gray-100'>
+          <h3 className='h-12 text-4xl font-semibold text-gray-800 dark:text-gray-200 line-clamp-2'>
             {story.title}
           </h3>
-          <p
-            className='mt-3 text-gray-700 text-md dark:text-gray-100 line-clamp-3'
-            dangerouslySetInnerHTML={{ __html: parsedDescription }}
-          />
+          <p className='mt-3 text-gray-700 text-md dark:text-gray-300 line-clamp-3'>
+            <MarkdownRenderer content={story.description} />
+          </p>
         </div>
 
         {/* Meta Info & Actions */}
-        <div className='flex items-center justify-between mt-4 text-gray-600 dark:text-gray-700'>
+        <div className='flex items-center justify-between mt-6 text-gray-600 dark:text-gray-700'>
+          <p className='dark:text-gray-400'>{story.authorName}</p>
           <div className='flex items-center gap-4'>
-            <span className='text-gray-600 dark:text-white'>
+            <span className='text-gray-600 dark:text-gray-400'>
               {new Date(story.createdAt).toLocaleDateString('en-US', {
                 year: 'numeric',
                 month: 'short',
@@ -88,21 +105,26 @@ const StoryCard = ({ story }: { story: Story }) => {
               })}
             </span>
             <span className='flex items-center gap-1'>
-              <FaHeart
-                className={`${isLiked ? 'text-red-500' : 'text-gray-600'} dark:text-white`}
+              <FaRegHeart
+                className={` dark:text-gray-400`}
+                // ${isLiked ? 'text-red-500' : 'text-gray-600'}
               />{' '}
-              {likeCount ? likeCount : ''}
+              <span className='text-gray-600 dark:text-gray-400'>
+                {likeCount ? likeCount : ''}
+              </span>
             </span>
             <span className='flex items-center gap-1'>
-              <FaRegComment className='text-gray-600 dark:text-white' />{' '}
-              {story.comments}
+              <FaRegComment className='text-gray-600 dark:text-gray-400' />{' '}
+              <span className='text-gray-600 dark:text-gray-400'>
+                {commentCount ? commentCount : ''}
+              </span>
             </span>
           </div>
 
-          <div className='flex items-center gap-2'>
+          {/* <div className='flex items-center gap-2'>
             <FaBookmark className='text-gray-600 dark:text-white' />
             <FaEllipsisV className='text-gray-600 dark:text-white' />
-          </div>
+          </div> */}
         </div>
       </div>
 
--- src/components/StoryEditor.tsx
+++ src/components/StoryEditor.tsx
@@ -3,25 +3,30 @@ import { useState } from 'react';
 import ReactMarkdownEditorLite from 'react-markdown-editor-lite';
 import 'react-markdown-editor-lite/lib/index.css'; // Import the styles
 
+interface StoryEditorProps {
+  onContentChange: (text: string) => void;
+  initialContent?: string;
+}
 
-const StoryEditor = ({ onContentChange, initialContent = '' }) => {
+const StoryEditor = ({
+  onContentChange,
+  initialContent = '',
+}: StoryEditorProps) => {
   const [content, setContent] = useState(initialContent);
   const mdParser = new MarkdownIt();
 
-  const handleEditorChange = ({ text }) => {
+  const handleEditorChange = ({ text }: { text: string }) => {
     setContent(text);
     onContentChange(text);
   };
 
   return (
-    // <div className='w-full max-w-4xl'>
-      <ReactMarkdownEditorLite
-        value={content}
-        style={{ height: '400px' }}
-        renderHTML={text => mdParser.render(text)}
-        onChange={handleEditorChange}
-      />
-    // </div>
+    <ReactMarkdownEditorLite
+      value={content}
+      style={{ height: '400px' }}
+      renderHTML={text => mdParser.render(text)}
+      onChange={handleEditorChange}
+    />
   );
 };
 
